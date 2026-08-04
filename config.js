4b0cf9795dca24ea8e80a1fef81ce5a544e62b4f
chrisvary-VC <Chris@varycreative.com>
2026-08-04 01:46:41 -0500

    Add dashboard configuration

---
+++ config.js
@@ -0,0 +1,17 @@
+window.DASHBOARD_CONFIG = {
+  networkName: "THE CAVE SPORTS NETWORK",
+  accent: "#ff4d1f",
+  accentAlt: "#ffc247",
+  timeFormat: "12h",
+  heroRotationMs: 12000,
+  storyRotationMs: 9000,
+  tickerSpeedSeconds: 44,
+  enabledSports: ["NFL", "NBA", "MLB", "F1", "INDYCAR"],
+  favorites: {
+    nfl: ["DAL"],
+    nba: [],
+    mlb: ["TEX"],
+    f1Drivers: [],
+    indycarDrivers: []
+  }
+};
